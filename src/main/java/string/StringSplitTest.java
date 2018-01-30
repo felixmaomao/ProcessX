c4ca5bd05edf46794752ebf866aa0a805ae2ed50
swfightforyl <swfightforyl@163.com>
2018-01-30 10:40:04 +0800

    no message

--- src/main/java/string/StringSplitTest.java
+++ src/main/java/string/StringSplitTest.java
@@ -2,6 +2,8 @@ package string;/**
  * Created by shenwei on 2017/5/16.
  */
 
+import java.util.Arrays;
+
 /**
  * @author shenwei
  * @create 2017-05-16
@@ -9,7 +11,11 @@ package string;/**
 public class StringSplitTest {
     public static void main(String[] args) {
         String str = "zzzzzzzzzzz";
-        System.out.println(str.split("A").length);
+        System.out.println(str.split("\\^").length);
         System.out.println(str.split("-")[0]);
+
+        System.out.println("33".replace("3", "胜"));
+
+        System.out.println(Arrays.stream("33".split("")).reduce((r1, r2) -> r1 + ":" + r2).get());
     }
 }
